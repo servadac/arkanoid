a29a4f8113f8d5b44f1f25256dc3ccf540ed62a2
Maciej Rawiński <maciej@Maciejs-MacBook-Pro.local>
2018-03-24 09:59:34 +0100

    start

---
+++ arkanoid.js
@@ -0,0 +1,79 @@
+(function () {
+    function createElementFromHtml(html) {
+        const div = document.createElement("div");
+        div.innerHTML = html;
+
+        return div.firstElementChild;
+    }
+
+    function Arkanoid() {
+
+    }
+
+    function Arena() {
+        this.arena = createElementFromHtml(this.html);
+        this.balls = [];
+        this.createBall();
+        this.paddleInst = new Paddle();
+        this.arena.appendChild(this.paddleInst.paddle);
+    }
+
+    Arena.prototype = {
+        html: `<div class="arena" ref="arena"></div>`,
+        createBall: function () {
+            const ballInst = new Ball();
+
+            this.arena.appendChild(ballInst.ball);
+            this.balls.push(ballInst);
+        }
+    };
+
+    function Bricks() {
+
+    }
+
+    function Brick() {
+
+    }
+
+    function Ball() {
+        this.ball = createElementFromHtml(this.html);
+    }
+    Ball.prototype = {
+        html: `<div class="ball" ref="ball"></div>`,
+        deltaX: 1,
+        deltaY: -1,
+        setPosition: function () {
+            const { ball, deltaX, deltaY } = this;
+
+            this.ballLeftPos = ball.offsetLeft + deltaX;
+            this.ballTopPos = ball.offsetTop + deltaY;
+            ball.style.top = this.ballTopPos + "px";
+            ball.style.left = this.ballLeftPos + "px";
+        },
+        getDiameter: function () {
+            return this.ball.offsetHeight;
+        },
+        getRadius: function () {
+            return this.getDiameter() / 2;
+        }
+    };
+
+    function Paddle() {
+        this.paddle = createElementFromHtml(this.html);
+    }
+    Object.assign(Paddle.prototype, {
+        html: `<div class="paddle" ref="paddle"></div>`,
+        getLeft: function () {
+            return this.paddle.offsetLeft;
+        },
+        getTop: function () {
+            return this.paddle.offsetTop;
+        },
+        getWidth: function () {
+            return this.paddle.offsetWidth;
+        }
+    });
+
+    document.getElementById("arkanoid").appendChild((new Arena()).arena);
+}());
